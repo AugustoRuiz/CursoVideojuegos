ebee38ce86b71b5dd418f6b2be3bacbb5f9337bd
Augusto Ruiz <augusto.ruiz@gmail.com>
2018-02-18 23:17:42 +0100

    Codigo de las tres primeras lecciones

--- lecc1/src/main.cpp
+++ lecc1/src/main.cpp
@@ -8,12 +8,12 @@ int main(int argc, char* argv[]) {
   // Code adapted from the SFML 2 "Window" example.
   cout << "Version " << VERSION_MAJOR << "." << VERSION_MINOR << endl;
 
-  sf::Window App(sf::VideoMode(800, 600), APP_NAME);
+  sf::RenderWindow App(sf::VideoMode(800, 600), APP_NAME);
   // Limpamos la ventana actual, de color negro.
-  App.clear(sf::Color::Black);
 
   while (App.isOpen()) {
     sf::Event Event;
+    App.clear(sf::Color::Black);
     while (App.pollEvent(Event)) {
       if (Event.type == sf::Event::Closed) {
 	       App.close();
